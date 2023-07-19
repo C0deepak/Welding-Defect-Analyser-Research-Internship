cb85d1f07e76ec76240e7093b4619356b13d5daa
akm-akm <adityanick08@gmail.com>
2023-07-19 22:11:22 +0530

    all works

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,45 @@ import Head from "next/head";
 import axios from "axios";
 import { useState } from "react";
 import { Image } from "next/image";
+
 function Index() {
 	const [errMsg, setErrMsg] = useState("");
-	const [img, setImg] = useState();
+	const [img, setImg] = useState(null);
+	const [img2, setImg2] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
-	const [url, setUrl] = useState();
 
 	const handleSubmit = async (e) => {
-		if (!img) {
+		e.preventDefault();
+		if (!img2) {
+			console.log(img2);
 			setErrMsg("Must Upload Image");
-		} else if (!img.type.includes("image")) {
-			setErrMsg("Upload image");
-			setImg();
 		} else {
 			try {
 				setIsLoading(true);
 				const formData = new FormData();
-				formData.append("img", img);
+				formData.append("file", img2);
 
 				const config = {
 					headers: {
 						"content-type": "multipart/form-data"
 					},
+					responseType: "blob",
 					withCredentials: false
 				};
-				const res = await axios.post("/api/yolo", formData, config);
-				const u = "http://localhost:3000/images/" + res.data.url;
-				setUrl(u);
-				console.log(url);
+				const res = await axios.post(
+					"http://127.0.0.1:5000/upload",
+
+					formData,
+					config
+				);
+				console.log(res.data);
+				const src = URL.createObjectURL(res.data);
+				setImg(src);
+
+				console.log(res.data);
 				setIsLoading(false);
 			} catch (err) {
+				console.log(err);
 				if (!err?.response) {
 					setErrMsg("No Internet connection");
 				} else if (err.response?.status === 400) {
@@ -50,24 +59,28 @@ function Index() {
 			<Head>
 				<title>Welding Defect Analyser</title>
 			</Head>
-			<div className="eventRegister">
+			<div className="a">
+				<h1>Welding Defect Analyser</h1>
 				<div className="heading">Upload Image</div>
 				<form encType="multipart/form-data" className="form">
 					<div className="file">
 						<input
 							onChange={(e) => {
-								setImg(e.target.files[0]);
-								handleSubmit();
+								setImg2(e.target.files[0]);
 							}}
 							name="img"
+							accept="image/*"
 							type="file"
 						/>
-
-						<div>
-							{url ? "" : <Image alt="Image" width="600px" src={url} />}
-						</div>
+						<button disabled={isLoading} onClick={(e) => handleSubmit(e)}>
+							Detect
+						</button>
 					</div>
 				</form>
+				{img ? <img width={"1200px"} src={img} alt="image" /> : ""}
+
+				{isLoading ? <h3>Loading...</h3> : ""}
+				{errMsg ? <h3>{errMsg}</h3> : ""}
 			</div>
 		</>
 	);
